73e6ec126cc3d2fe26d260e4b6e98906d4ada6b2
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-08-23 12:11:58 -0700

    installed various dependencies.

--- config/deploy.js
+++ config/deploy.js
@@ -3,23 +3,38 @@
 
 module.exports = function(deployTarget) {
   let ENV = {
-    build: {}
-    // include other plugin configuration that applies to all deploy targets here
+    build: {},
+    pipeline: {
+      // This setting runs the ember-cli-deploy activation hooks on every deploy
+      // which is necessary in order to run ember-cli-deploy-cloudfront.
+      // To disable CloudFront invalidation, remove this setting or change it to `false`.
+      // To disable ember-cli-deploy-cloudfront for only a particular environment, add
+      // `ENV.pipeline.activateOnDeploy = false` to an environment conditional below.
+      activateOnDeploy: true
+    },
+    s3: {
+      accessKeyId: process.env.AWS_KEY,
+      secretAccessKey: process.env.AWS_SECRET,
+      filePattern: '*'
+    },
+    cloudfront: {
+      accessKeyId: process.env.AWS_KEY,
+      secretAccessKey: process.env.AWS_SECRET
+    }
   };
-
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
-  }
 
   if (deployTarget === 'staging') {
     ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
+    ENV.s3.bucket = process.env.STAGING_BUCKET;
+    ENV.s3.region = process.env.STAGING_REGION;
+    ENV.cloudfront.distribution = process.env.STAGING_DISTRIBUTION;
   }
 
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
+    ENV.s3.bucket = process.env.PRODUCTION_BUCKET;
+    ENV.s3.region = process.env.PRODUCTION_REGION;
+    ENV.cloudfront.distribution = process.env.PRODUCTION_DISTRIBUTION;
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
