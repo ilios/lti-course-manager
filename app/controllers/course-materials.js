2662060916a9faf49f828c86563e4f03ba6851c2
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-10-22 15:50:04 -0700

    copied controllers from common.

---
+++ app/controllers/course-materials.js
@@ -0,0 +1,4 @@
+import Controller from '@ember/controller';
+import MaterialsController from 'ilios-common/mixins/course-materials-controller';
+
+export default Controller.extend(MaterialsController);
