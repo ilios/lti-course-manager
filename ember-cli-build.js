9e801c2819de36a705687031b23fb7de683cd3ed
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-02-25 15:55:29 -0800

    c/p more build options from frontend.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,24 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+  let env = EmberApp.env() || 'development';
+  let isProductionLikeBuild = ['production', 'staging', 'preview'].indexOf(env) > -1;
+
+  
   let app = new EmberApp(defaults, {
     fingerprint: {
       exclude: ['ilios-icon.png']
+    },
+    sourcemaps: {
+      enabled: true,
+    },
+    minifyCSS: { enabled: isProductionLikeBuild },
+    minifyJS: { enabled: isProductionLikeBuild },
+
+    tests: env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
+    hinting: env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
+    babel: {
+      plugins: [ require('ember-auto-import/babel-plugin') ]
     },
     'ember-cli-babel': {
       includePolyfill: true
@@ -14,6 +29,29 @@ module.exports = function(defaults) {
       languages: ['fr','es'],
       plugins: ['lists', 'code_view', 'link'],
       themes: 'gray'
+    },
+    postcssOptions: {
+      compile: {
+        extension: 'scss',
+        enabled: true,
+        parser: require('postcss-scss'),
+        plugins: [
+          {
+            module: require('@csstools/postcss-sass'),
+          },
+        ]
+      },
+      filter: {
+        enabled: true,
+        plugins: [
+          {
+            module: require('autoprefixer'),
+          }
+        ]
+      }
+    },
+    autoImport: {
+      publicAssetURL: '/assets'
     },
   });
 
