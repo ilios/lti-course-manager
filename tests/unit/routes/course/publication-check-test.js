4a0404ac10abb090b0a602e8b0e2b92c4303c3ef
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-02-28 16:22:13 -0800

    fix up unit test with new route names.

--- tests/unit/routes/course/publication-check-test.js
+++ tests/unit/routes/course/publication-check-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | course/publicationCheck', function(hooks) {
+module('Unit | Route | course/publication_check', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:course/publication-check');
+    let route = this.owner.lookup('route:course/publication_check');
     assert.ok(route);
   });
 });
--- tests/unit/routes/course/session/publication-check-test.js
+++ tests/unit/routes/course/session/publication-check-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | course/session/publicationCheck', function(hooks) {
+module('Unit | Route | course/session/publication_check', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:course/session/publication-check');
+    let route = this.owner.lookup('route:course/session/publication_check');
     assert.ok(route);
   });
 });
