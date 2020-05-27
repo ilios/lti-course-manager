981ee9d9b2e1d58ca83c320f748c5d1416572843
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-05-27 11:17:05 -0700

    realigns eslint config with frontend.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
+'use strict';
+
 module.exports = {
   root: true,
   extends: [
     '@ilios/eslint-config-ember'
   ],
   rules: {
+    'ember/use-ember-get-and-set': 0,
+    'ember/no-jquery': 0,
+    'ember/use-brace-expansion': 0,
+    'ember/no-new-mixins': 0,
+    'ember/no-capital-letters-in-routes': 0,
+    'ember/no-unnecessary-route-path-option': 0,
     'ember/routes-segments-snake-case': 0,
-    "node/no-extraneous-require": ["error", {
-        "allowModules": ["postcss-scss"]
-    }],
-    'sort-imports': 0,
+    'sort-imports': 0
   }
 };
